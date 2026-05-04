16f3f104ec035bd03a7f0f209772a44afaa2f265
Manav Pal <manav96544@gmail.com>
2026-05-04 22:12:59 +0530

    feat: update database configuration to use environment variables

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user:"manavapp",
-  password:"123456",
-  database:"quickblog",
+  host: process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  port:process.env.DB_PORT,
   waitForConnections:true,
   connectionLimit:10
 });
